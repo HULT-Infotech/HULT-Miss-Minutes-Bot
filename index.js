76a68a3d2b7469dfa0bf0ad24c388a915fe9571f
TOM SABU <tomsabu0000@gmail.com>
2024-12-19 19:17:56 +0530

    Update configuration comments for clarity on environment variables

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const config = require('./config.json');
 require('dotenv').config();
 
 // Set environment variables
-config.TOKEN = process.env.DISCORD_TOKEN;
-config.GUILD_ID = process.env.GUILD_ID;
-config.ROLE_ID = process.env.ROLE_ID;
-config.VC_CATEGORIES = process.env.VC_CATEGORIES.split(',');
+config.TOKEN = process.env.DISCORD_TOKEN;   //! Change this to your bot's token
+config.GUILD_ID = process.env.GUILD_ID;   //! Change this to your server's ID
+config.ROLE_ID = process.env.ROLE_ID;  //! Change this to your role's ID
+config.VC_CATEGORIES = process.env.VC_CATEGORIES.split(','); //! Change this to your voice channel category IDs
 
 const client = new Client({
     intents: [
